7b036ef00491b6f0a09daa583888a2b58f54f51c
Record-Street <miguelbaronembb@gmail.com>
2025-10-12 15:58:45 -0300

    Update ZERX1.py
    Adição de feedback visual com cores e limites para valores

--- ZERX1.py
+++ ZERX1.py
@@ -3,112 +3,145 @@ import mediapipe as mp
 import time
 import math
 
-# Initialize webcam
+# Nomes para as landmarks utilizadas
+REFERENCE = 0
+REFERENCE2 = 17
+THUMB_LM = 4
+INDEX_LM = 8
+MIDDLE_LM = 12
+RING_LM = 16
+PINK_LM = 20
+
+# Inicializa a camera
 cap = cv2.VideoCapture(0)
 
-# Set up MediaPipe Hands
+# Configura mãos do mediapipe
 mpHands = mp.solutions.hands
-hands = mpHands.Hands(False, 1)  # False for video, 1 hand max
-mpDraw = mp.solutions.drawing_utils  # For drawing landmarks
+hands = mpHands.Hands(False, 1)
+mpDraw = mp.solutions.drawing_utils
 
-# Variables for FPS calculation
+# Variáveis para calcular FPS
 pTime = 0  # Previous time
 cTime = 0  # Current time
 
-#dictionary in which the coordinates are stored
+# Dicionário em que são guardadas as posições
 Dict = {}
 
+setupStage = "closed"
 
-usedLandmarks = [0, 2, 4, 5, 9, 13 ,17, 8, 12, 16, 20]
-Fcx1 = 0
-Fcy1 = 0
-Fcx2 = 0
-Fcy2 = 0
-Fcx = 0
-Fcy = 0
-list1 = 0
-list2 = 0
+# Lista de pontos da mão usados no programa
+usedLandmarks = [REFERENCE, REFERENCE2, THUMB_LM, INDEX_LM, MIDDLE_LM, RING_LM, PINK_LM]
 
-
-#function to plot new points based on existing ones
-def createpoint(id1, id2, prop1, prop2, newid):
-    Fcx1 = ((Dict[str(id1)])[0])
-    Fcy1 = ((Dict[str(id1)])[1])
-    Fcx2 = ((Dict[str(id2)])[0])
-    Fcy2 = ((Dict[str(id2)])[1])
-    Fcx = Fcx1 * prop1 + Fcx2 * prop2
-    Fcy = Fcy1 * prop1 + Fcy2 * prop2
-    Dict[str(newid)] = [Fcx, Fcy]
-
+# Função para calcular módulo de distância entre 2 pontos
 def diagonalDistance(point1, point2):
     list1 = Dict[point1]
     list2 = Dict[point2]
     return math.sqrt((list1[0] - list2[0])**2 + (list1[1] - list2[1])**2)
 
-
-
-# Main loop
 while True:
-    #reset dictionary
-    Dict = {}
-
-    # Read frame from webcam
+    # Leitura da frame pela webcam
     success, img = cap.read()
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert to RGB for MediaPipe
-
-    # Process image to detect hands
+    # Conversão da imagem para RGB
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # Processar mãos na imagem
     results = hands.process(imgRGB)
 
-    # If hands are detected
+    # Se mãos forem detectadas, passa por cada ponto
     if results.multi_hand_landmarks:
+        # Passa por cada resultado da mão
         for handLms in results.multi_hand_landmarks:
+            # Passa por cada ponto e ID do resultado
             for id, lm in enumerate(handLms.landmark):
-                    #if we need the landmark
+                    # Caso o landmark seja usado
                     if id in usedLandmarks:
-                        # Convert normalized coordinates to pixel values
-                        h, w, c = img.shape
-                        cx, cy = int(lm.x * w), int(lm.y * h)
+                        # Extrai os valores de pixel da imagem
+                        height, width, depth = img.shape
+                        # "Normaliza" coordenadas x e y usando os valores de pixel
+                        coorX, coorY = int(lm.x * width), int(lm.y * height)
+                        # Adiciona os valores x e y com a chave do ID no dicionário
+                        Dict[str(id)] = [coorX, coorY]
 
-                        #add value to dictionary
-                        Dict[str(id)] = [cx, cy]
 
-                        #draw cool circle
-                        cv2.circle(img, (int(cx), int(cy)), 7, (255, 100, 0), cv2.FILLED)
 
-            # Draw landmarks and connections on the image
+            # Desnha conexões entre os pontos da mão
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
-            # create reference values
-            createpoint(0, 5, 0.1, 0.9, "indexR")
-            createpoint(0, 9, 0.1, 0.9, "middleR")
-            createpoint(0, 13, 0.1, 0.9, "ringR")
-            createpoint(0, 17, 0.1, 0.9, "pinkyR")
+            # Obtém referência para comparação
+            reference = diagonalDistance("17","0")
 
-            #circle reference points
-            cv2.circle(img, (int((Dict["middleR"])[0]), int((Dict["middleR"])[1])), 15, (255, 100, 255), cv2.FILLED)
-            cv2.circle(img, (int((Dict["indexR"])[0]), int((Dict["indexR"])[1])), 15, (255, 100, 255), cv2.FILLED)
-            cv2.circle(img, (int((Dict["ringR"])[0]), int((Dict["ringR"])[1])), 15, (255, 100, 255), cv2.FILLED)
-            cv2.circle(img, (int((Dict["pinkyR"])[0]), int((Dict["pinkyR"])[1])), 15, (255, 100, 255), cv2.FILLED)
+            # Pega posições atuais de cada dedo(corrigidas com a referência)
+            finger1 = diagonalDistance("4", "17") / reference
+            finger2 = diagonalDistance("8", "0") / reference
+            finger3 = diagonalDistance("12", "0") / reference
+            finger4 = diagonalDistance("16", "0") / reference
+            finger5 = diagonalDistance("20", "0") / reference
 
-            #create % values of each finger
+            # Calcula valor final para cada dedo
+            if setupStage == "done":
+                dedao = (finger1 - finger1Closed) / (finger1Open - finger1Closed)
+                if dedao < 0:
+                    dedao = 0
+                if dedao > 1:
+                    dedao = 1
+                indicador = (finger2 - finger2Closed) / (finger2Open - finger2Closed)
+                if indicador < 0:
+                    indicador = 0
+                if indicador > 1:
+                    indicador = 1
+                meio = (finger3 - finger3Closed) / (finger3Open - finger3Closed)
+                if meio < 0:
+                    meio = 0
+                if meio > 1:
+                    meio = 1
+                anelar = (finger4 - finger4Closed) / (finger4Open - finger4Closed)
+                if anelar < 0:
+                    anelar = 0
+                if anelar > 1:
+                    anelar = 1
+                mindinho = (finger5 - finger5Closed) / (finger5Open - finger5Closed)
+                if mindinho < 0:
+                    mindinho = 0
+                if mindinho > 1:
+                    mindinho = 1
+                # Printa valores finais
+                print(round(dedao, 3), round(indicador, 3), round(meio, 3), round(anelar, 3), round(mindinho, 3))
+                # Adiciona cores para indicar o estado do dedo
+                cv2.circle(img, (int((Dict["4"])[0]), int((Dict["4"])[1])), 20, (0, int(dedao*255), int(255 - dedao*255)), -1, cv2.FILLED)
+                cv2.circle(img, (int((Dict["8"])[0]), int((Dict["8"])[1])), 20, (0, int(indicador*255), int(255 - indicador*255)), -1, cv2.FILLED)
+                cv2.circle(img, (int((Dict["12"])[0]), int((Dict["12"])[1])), 20, (0, int(meio*255), int(255 - meio*255)), -1, cv2.FILLED)
+                cv2.circle(img, (int((Dict["16"])[0]), int((Dict["16"])[1])), 20, (0, int(anelar*255), int(255 - anelar*255)), -1, cv2.FILLED)
+                cv2.circle(img, (int((Dict["20"])[0]), int((Dict["20"])[1])), 20, (0, int(mindinho*255), int(255 - mindinho*255)), -1, cv2.FILLED)
 
-            indexValue = diagonalDistance("indexR", "8")  / diagonalDistance("0", "9")
-            middleValue = diagonalDistance("middleR", "12") / diagonalDistance("0", "9")
-            ringValue = diagonalDistance("ringR", "16") / diagonalDistance("0", "9")
-            pinkyValue = diagonalDistance("pinkyR", "20") / diagonalDistance("0", "9")
-            print(round(indexValue, 3), round(middleValue, 3), round(ringValue, 3) , round(pinkyValue, 3))
 
-    # Calculate FPS
+    # Calcula FPS
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
 
-    # Display FPS on the image
+    # Mostra o FPS na imagem
     cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
                 (255, 0, 255), 3)
 
-    # Show the image
+    # Mostra a imagem
     cv2.imshow("Image", img)
 
-    # Wait for 1ms and check for 'q' key to exit
-    cv2.waitKey(1)
+    # Espera pela tecla "q" para registrar estados
+    if cv2.waitKey(10) == ord('q'):
+        if setupStage == "closed":
+            # Registra os valores da mão fechada
+            finger1Closed = diagonalDistance("4", "17") / reference
+            finger2Closed = diagonalDistance("8", "0") / reference
+            finger3Closed = diagonalDistance("12", "0") / reference
+            finger4Closed = diagonalDistance("16", "0") / reference
+            finger5Closed = diagonalDistance("20", "0") / reference
+            setupStage = "open"
+            print("closed state registered!")
+        elif setupStage == "open":
+            # Registra os valores pra mão aberta
+            finger1Open = diagonalDistance("4", "17") / reference
+            finger2Open = diagonalDistance("8", "0") / reference
+            finger3Open = diagonalDistance("12", "0") / reference
+            finger4Open = diagonalDistance("16", "0") / reference
+            finger5Open = diagonalDistance("20", "0") / reference
+            setupStage = "done"
+            print("open state registered!")
